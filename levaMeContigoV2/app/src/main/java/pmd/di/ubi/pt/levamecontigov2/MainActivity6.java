b4d0166c1064ecbac383bd2d5228db4e0d1e59d2
yoguimachine <44408326+yoguimachine@users.noreply.github.com>
2020-12-14 21:56:02 +0000

    qr scanner

--- levaMeContigoV2/app/src/main/java/pmd/di/ubi/pt/levamecontigov2/MainActivity6.java
+++ levaMeContigoV2/app/src/main/java/pmd/di/ubi/pt/levamecontigov2/MainActivity6.java
@@ -1,8 +1,10 @@
 package pmd.di.ubi.pt.levamecontigov2;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.Color;
@@ -11,9 +13,16 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.budiyev.android.codescanner.CodeScanner;
+import com.budiyev.android.codescanner.CodeScannerView;
+import com.budiyev.android.codescanner.DecodeCallback;
+import com.google.zxing.Result;
 import com.google.zxing.WriterException;
 
 import androidmads.library.qrgenearator.QRGContents;
@@ -24,6 +33,10 @@ public class MainActivity6 extends AppCompatActivity {
     ImageButton gerar, ler;
     Animation anim;
     ImageView qrvalue;
+    CodeScanner codeScanner;
+    CodeScannerView scannerView;
+    TextView qr;
+
 
     @SuppressLint("ClickableViewAccessibility")
     @Override
@@ -32,9 +45,14 @@ public class MainActivity6 extends AppCompatActivity {
 
         setContentView(R.layout.activity_main6);
 
-        gerar = (ImageButton)findViewById(R.id.gerar);
-        ler = (ImageButton)findViewById(R.id.ler);
+        gerar = (ImageButton) findViewById(R.id.gerar);
+        ler = (ImageButton) findViewById(R.id.ler);
         qrvalue = findViewById(R.id.qrcodeholder);
+        scannerView = findViewById(R.id.scannerView);
+        qr = findViewById(R.id.QR);
+        scannerView.setVisibility(View.INVISIBLE);
+        codeScanner = new CodeScanner(this, scannerView);
+
 
         gerar.setVisibility(View.VISIBLE);
 
@@ -52,16 +70,35 @@ public class MainActivity6 extends AppCompatActivity {
                 try {
                     Bitmap qrbits = qrencoder.getBitmap();
                     qrvalue.setImageBitmap(qrbits);
-                } catch(Exception e){
+                    gerar.setImageResource(R.drawable.generate);
+                } catch (Exception e) {
                     e.printStackTrace();
                 }
             }
         });
 
-        ler.setOnClickListener(new View.OnClickListener() {
+        ler.setOnClickListener(v -> checkqr());
+        System.out.println(qr.getText().toString());
+
+    }
+
+    public void checkqr() {
+        ler.setImageResource(R.drawable.read2);
+        scannerView.setVisibility(View.VISIBLE);
+        codeScanner.startPreview();
+        codeScanner.setDecodeCallback(new DecodeCallback() {
             @Override
-            public void onClick(View v) {
-                ler.setImageResource(R.drawable.read2);
+            public void onDecoded(@NonNull Result result) {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        qr.setText(result.getText().toString());
+                        codeScanner.stopPreview();
+                        scannerView.setVisibility(View.INVISIBLE);
+                        codeScanner.releaseResources();
+                        ler.setImageResource(R.drawable.read);
+                    }
+                });
             }
         });
 
